6643786dd5efc1a6d5a83f0999f41a91783cdbcf
Jordan Smith <jordan.smith@tpx.com>
2024-09-25 14:28:29 -0600

    sending sms to rmq

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import logging
 from fastapi import FastAPI, Request
 from nba_api_client import get_lineups_by_team
-from sms import print_message
+from sms import forward_message
 from fastapi.responses import JSONResponse
 
 logging.basicConfig(level=logging.DEBUG)
@@ -21,7 +21,8 @@ async def ping():
 
 @app.post("/sms/")
 async def sms(data: dict):
-    print(f"Received SMS: {data['data']['payload']['text']}")
-    print(f"From: {data['data']['payload']['from']['phone_number']}")
+    logging.info(f"Received SMS: {data['data']['payload']['text']}")
+    logging.info(f"From: {data['data']['payload']['from']['phone_number']}")
+    forward_message(data['data']['payload']['from']['phone_number'], data['data']['payload']['text'])
 
     return JSONResponse(content={"message": "Message received."}, status_code=200)
--- src/sms.py
+++ src/sms.py
@@ -1,4 +1,28 @@
+from pika import BlockingConnection, URLParameters
 
 
-def print_message(message):
-    print(message)
+def forward_message(from_user: str, message: str):
+    """
+    Forward the message to the agent queue.
+    :param from_user: The user's phone number.
+    :param message: The message to forward.
+    :return:
+    """
+
+    parameters = URLParameters(url="amqp://agents:guppyfishaiagent!!@164.92.120.177:5672")
+    connection = BlockingConnection(parameters)
+    channel = connection.channel()
+
+    channel.queue_declare(queue="tlc")
+    channel.queue_bind(exchange='ai', queue="tlc", routing_key="ai.tlc")
+
+    message = {
+        "from": from_user,
+        "message": message
+    }
+
+    channel.basic_publish(exchange='ai', routing_key="ai.tlc", body=str(message))
+
+    connection.close()
+
+    return True
